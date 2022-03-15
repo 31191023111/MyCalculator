c20a39781d00d7a5694e227a582001ba3fdc9a3e
Dang Cong Binh <40337205+congbinh75@users.noreply.github.com>
2022-03-15 16:51:08 +0700

    Added for the first time

--- obj/Debug/120/android/environment.armeabi-v7a.s
+++ obj/Debug/120/android/environment.armeabi-v7a.s
@@ -105,7 +105,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"274bbecc-c449-4a76-9332-ad0e06e1833b"
+	.asciz	"ff170243-f4ad-42e4-88dd-e335651973b9"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
